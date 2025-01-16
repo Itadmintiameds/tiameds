75ce57153c3fe9e80772cb16edbaf53c65bb67ad
abhishek kumar <abhishekritu810219@gmail.com>
2025-01-16 13:49:31 +0530

    check url working

--- apps/home/next.config.mjs
+++ apps/home/next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
-        return [
-            
-            {
-                source: '/lab/:path*',
-                destination: 'https://tiameds-lab-app.vercel.app/:path*', // Proxy with path forwarding
-            },
-        ];
+      return [
+        {
+          source: '/lab/:path*',
+          destination: 'https://tiameds-lab-app.vercel.app/:path*', // Ensure dynamic paths are proxied
+        },
+        {
+          source: '/bill/:path*',
+          destination: 'https://tiameds-bill-app.vercel.app/:path*',
+        },
+      ];
     },
-}
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
+  
