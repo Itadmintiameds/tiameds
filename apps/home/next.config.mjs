7025d7081ddb892dbc9530735b5c2cee1862eba9
abhishek kumar <abhishek@tiameds.ai>
2025-01-16 13:00:22 +0530

    next.config.mjs in home

--- apps/home/next.config.mjs
+++ apps/home/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/lab/:path*',
+            destination: 'https://tiameds-lab-app.vercel.app/:path*', // Proxy to the lab app
+          },
+          
+        ];
+      },
+}
 
 export default nextConfig;
