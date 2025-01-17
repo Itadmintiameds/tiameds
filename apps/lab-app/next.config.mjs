6a647ec4520bfd3ea247c6e4693e6f6da884db4b
abhishek kumar <abhishekritu810219@gmail.com>
2025-01-17 10:15:07 +0530

    check domain

--- apps/lab-app/next.config.mjs
+++ apps/lab-app/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    basePath: '/lab',  // Configure the base path
-    trailingSlash: true,
+    reactStrictMode: true
 };
 
 export default nextConfig;
