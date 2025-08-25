e02865e91cf87281803b18bd8eab03a17d78da2c
abhishek kumar <abhishekritu810219@gmail.com>
2025-08-25 15:29:14 +0530

    remove duplicate app

--- apps/apps/home/src/app/page/404.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const pageNotFound = () => {
-  return (
-    <div>this is 404 page</div>
-  )
-}
-
-export default pageNotFound
--- apps/apps/home/src/app/types/casestudies.ts
+++
@@ -1,7 +0,0 @@
-export interface CaseStudy {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    link: string;
-}
