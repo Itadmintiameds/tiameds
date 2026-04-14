cb3d526edcb123958d56db413fd759541e147e37
ShreerakshaRao <your_github_shreeraksha@tiameds.ai>
2026-04-14 10:11:59 +0530

    Testing branch undone

--- apps/lab-app/src/app/(admin)/dashboard/page.tsx
+++ apps/lab-app/src/app/(admin)/dashboard/page.tsx
@@ -13,7 +13,7 @@ import Technacian from './sample/_component/technican/Technacian';
 const tabs = [
   {
     id: 'patient',
-    label: 'Patient Managementt',
+    label: 'Patient Management',
     icon: <FaPerson className="text-lg" />,
     activeColor: 'text-purple-600',
     borderColor: 'bg-purple-600',
