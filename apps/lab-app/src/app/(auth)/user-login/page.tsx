097579b164bfb63bf2ab2278563d44db09735f7b
Sunny Kumar <dev@tiameds.ai>
2026-08-31 15:21:41 +0530

    sgn up removed

--- apps/lab-app/src/app/(auth)/user-login/page.tsx
+++ apps/lab-app/src/app/(auth)/user-login/page.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { 
   FaArrowLeft, FaSignInAlt,FaFlask, 
   FaMicroscope, FaLock, FaUser, FaEye, FaEyeSlash, FaEnvelope,
-  FaUserPlus
+  // FaUserPlus
 } from 'react-icons/fa'
 // import {FaUserPlus} from 'react-icons/fa';
 import { FiLoader } from 'react-icons/fi'
@@ -474,7 +474,7 @@ const LoginPage: React.FC = () => {
           )}
 
           {/* Divider */}
-          <div className="relative">
+          {/* <div className="relative">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-200" />
             </div>
@@ -488,7 +488,7 @@ const LoginPage: React.FC = () => {
               <FaUserPlus className="text-purple-600" />
               Register Laboratory
             </button>
-          </Link>
+          </Link> */}
 
           <p className="text-center text-xs text-gray-400">
             © {new Date().getFullYear()} Tiamed Diagnostics. All rights reserved.
